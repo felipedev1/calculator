076d0e385c8d99f171a803c176809ddb0712747e
Felipe <felipe.pereira.dev@gmail.com>
2020-06-17 10:04:00 -0300

    implementing the numeric field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap'
 import './App.css';
 
 function App() {
+
+  const [txtNumbers, setTxtNumbers] = useState('0');
+
+  function addNumber(number){
+    setTxtNumbers(txtNumbers + number)
+  }
+
+  function defineOperation(operation) {
+    setTxtNumbers(operation)
+  }
+
   return (
     <Jumbotron style={{
       background: 'transparent !important',
@@ -20,64 +31,91 @@ function App() {
           <Col xs="9">
             <Form.Control type="text"
               name="txtNumbers"
-              class="text-right"
+              className="text-right"
+              value={txtNumbers}
               readOnly />
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button variant="light">7</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('7')} 
+            >7</Button>
           </Col>
           <Col>
-            <Button variant="light">8</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('8')} 
+            >8</Button>
           </Col>
           <Col>
-            <Button variant="light">9</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('9')} 
+            >9</Button>
           </Col>
           <Col>
-            <Button variant="warning">/</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button variant="light">4</Button>
-          </Col>
-          <Col>
-            <Button variant="light">5</Button>
-          </Col>
-          <Col>
-            <Button variant="light">6</Button>
-          </Col>
-          <Col>
-            <Button variant="warning">*</Button>
+            <Button variant="warning" 
+              onClick={() => defineOperation('/')} >/</Button>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button variant="light">1</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('4')} 
+            >4</Button>
           </Col>
           <Col>
-            <Button variant="light">2</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('5')} 
+            >5</Button>
           </Col>
           <Col>
-            <Button variant="light">3</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('6')} 
+            >6</Button>
           </Col>
           <Col>
-            <Button variant="warning">-</Button>
+            <Button variant="warning" 
+              onClick={() => defineOperation('*')} >*</Button>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button variant="light">0</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('1')} 
+            >1</Button>
           </Col>
           <Col>
-            <Button variant="light">.</Button>
+            <Button variant="light" 
+              onClick={(e) => addNumber('2')} 
+            >2</Button>
+          </Col>
+          <Col>
+            <Button variant="light" 
+              onClick={(e) => addNumber('3')} 
+            >3</Button>
+          </Col>
+          <Col>
+            <Button variant="warning" 
+              onClick={() => defineOperation('-')} >-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light" 
+              onClick={(e) => addNumber('0')} 
+            >0</Button>
+          </Col>
+          <Col>
+            <Button variant="light" 
+              onClick={(e) => addNumber()} 
+            >.</Button>
           </Col>
           <Col>
             <Button variant="success">=</Button>
           </Col>
           <Col>
-            <Button variant="warning">+</Button>
+            <Button variant="warning" 
+              onClick={() => defineOperation('+')} >+</Button>
           </Col>
         </Row>
       </Container>
